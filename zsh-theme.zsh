20022ef2ab732dccb1d1d3a0dcf2db434abf28f3
Samantha McVey <samantham@posteo.net>
2019-02-21 21:10:50 +0100

    Format the || nicer on multiple lines

--- zsh-theme.zsh
+++ zsh-theme.zsh
@@ -17,8 +17,9 @@ ZSH_THEME_GIT_PROMPT_UNSTAGED="%{$fg_bold[yellow]%}●%{$reset_color%}"
 ZSH_THEME_GIT_PROMPT_UNTRACKED="%{$fg_bold[red]%}●%{$reset_color%}"
 
 bureau_git_branch () {
-  ref=$(command git symbolic-ref HEAD 2> /dev/null) || \
-  ref=$(command git rev-parse --short HEAD 2> /dev/null) || return
+  ref=$(command git symbolic-ref HEAD 2> /dev/null) \
+  || ref=$(command git rev-parse --short HEAD 2> /dev/null) \
+  || return
   print -r "${ref#refs/heads/}"
 }
 
